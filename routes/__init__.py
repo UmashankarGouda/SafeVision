8693f80f1beab71603b1b43783962c35f348a9e3
SATYAM GUPTA <143061930+Satyamgupta2365@users.noreply.github.com>
2025-05-13 16:49:14 +0530

    __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,5 +1,4 @@
 from .pages_routes import pages_bp
 from .surveillance_routes import surveillance_bp
-from .analytics import analytics_bp  # ✅ Correct
-
+from .analytics import analytics_bp  
 __all__ = ['pages_bp', 'surveillance_bp', 'analytics_bp']
