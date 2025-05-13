c856f2e59dd809b0ebfd91c6c981af1934e1bf3e
SATYAM GUPTA <143061930+Satyamgupta2365@users.noreply.github.com>
2025-05-13 16:27:40 +0530

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 import config
-from routes import pages_bp, surveillance_bp, analytics_bp  
+from routes import pages_bp, surveillance_bp, analytics_bp
 
 def create_app():
     app = Flask(__name__)
+
+    # Register blueprints
     app.register_blueprint(surveillance_bp)
     app.register_blueprint(pages_bp)
-    app.register_blueprint(analytics_bp)  
+    app.register_blueprint(analytics_bp)
+
     return app
 
 if __name__ == '__main__':
